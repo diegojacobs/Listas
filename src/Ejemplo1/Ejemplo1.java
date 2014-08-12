6a618e9adac04fee57a6ddd787616647bcd509f0
Usuario <Usuario@VAIO>
2014-08-12 16:36:38 -0600

    factory

--- src/Ejemplo1/Ejemplo1.java
+++ src/Ejemplo1/Ejemplo1.java
@@ -19,21 +19,21 @@ public class Ejemplo1 extends Sorting {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Listas<Integer> _lista = new Listas<Integer>();
+        Listas<Integer> _lista = null;
         Nodo<Integer>[] list;
+        ListasFactory Factory = new ListasFactory();
         //list = new Nodo<Integer>(8);
-        list =  (Nodo[])Array.newInstance(Nodo.class, 8);
-        list[0] = new Nodo<Integer>(1);
-        list[1] = new Nodo<Integer>(2);
-        list[2] = new Nodo<Integer>(3);
-        list[3] = new Nodo<Integer>(4);
-        list[4] = new Nodo<Integer>(5);
-        list[5] = new Nodo<Integer>(6);
-        list[6] = new Nodo<Integer>(7);
-        list[7] = new Nodo<Integer>(8);
         Scanner _in  = new Scanner(System.in);
         
         int opcion =0;
+        
+        
+        System.out.print("Tipo de Lista que desea trabajar: \n");
+        System.out.print("1. Simple\n");
+        System.out.print("2. Doble\n");
+        System.out.print("3. Circular\n");
+        opcion = _in.nextInt();
+        Listas _list = Factory.getClass(opcion);
         
         while (opcion!= 4){
             System.out.print("1. Agregar\n");
@@ -54,8 +54,8 @@ public class Ejemplo1 extends Sorting {
                     System.out.print("\n");
                     break;    
                 case 3:
-                    Sorting.selectionSort(list);
-                    Sorting.insertionSort(list);
+                    Sorting.selectionSort(_list);
+                    Sorting.insertionSort(_list);
                     System.out.print("La lista se ha ordenado.");
                     System.out.print("\n");
                     break;
